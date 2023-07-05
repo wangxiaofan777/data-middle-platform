a88a726438f494dd92bfae603ee0c47fce1beeb5
wangmaosong <wangmaosong@oceanum.cn>
2023-07-05 18:39:08 +0800

    feat: add

---
+++ datasource/src/main/java/com/wxf/datasource/DatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.wxf.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourceApplication.class, args);
+    }
+
+}
---
+++ user/src/test/java/com/wxf/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxf.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
