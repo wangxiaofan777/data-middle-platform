a52b715565309f816fc726f6c50a165a488fba22
wangmaosong <wangmaosong@oceanum.cn>
2023-07-12 12:08:04 +0800

    feat: add module

---
+++ model-service/src/main/java/com/wxf/modelservice/ModelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wxf.modelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelServiceApplication.class, args);
+    }
+
+}
---
+++ model-service/src/test/java/com/wxf/modelservice/ModelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxf.modelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
