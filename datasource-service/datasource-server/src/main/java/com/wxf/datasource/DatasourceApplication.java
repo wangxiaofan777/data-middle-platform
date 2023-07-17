0e7bf3be61a32e6168337f695ef47eb0bfd77949
wangmaosong <wangmaosong@oceanum.cn>
2023-07-17 19:10:22 +0800

    feat: mybatis plus

--- datasource-service/datasource-server/src/main/java/com/wxf/datasource/DatasourceApplication.java
+++ datasource-service/datasource-server/src/main/java/com/wxf/datasource/DatasourceApplication.java
@@ -1,9 +1,15 @@
 package com.wxf.datasource;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@MapperScan(basePackages = {
+        "com.wxf.datasource.mapper"
+})
+@SpringBootApplication(scanBasePackages = {
+        "com.wxf.datasource"
+})
 public class DatasourceApplication {
 
     public static void main(String[] args) {
