49095d7eca9b47e24d0faf91754aa97694da43b3
wangmaosong <wangmaosong@oceanum.cn>
2023-09-11 15:21:14 +0800

    feat: 监控

--- user-service/user-server/src/main/java/com/wxf/user/IndexController.java
+++ user-service/user-server/src/main/java/com/wxf/user/IndexController.java
@@ -1,5 +1,6 @@
 package com.wxf.user;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Timed(value = "首页", description = "首页耗时", histogram = true)
     @GetMapping
     public ResponseEntity<String> index() {
         return ResponseEntity.ok("Hello World");
--- user-service/user-server/src/main/java/com/wxf/user/config/SecurityConfig.java
+++ user-service/user-server/src/main/java/com/wxf/user/config/SecurityConfig.java
@@ -1,18 +1,12 @@
 package com.wxf.user.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    /*@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -20,5 +14,5 @@ public class SecurityConfig {
                         .authenticated())
                 .oauth2Login(Customizer.withDefaults());
         return http.build();
-    }
+    }*/
 }
