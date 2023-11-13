3a946f06dc2011b78e4f43a3e1818cda020e5d8b
wangmaosong <wangmaosong@oceanum.cn>
2023-11-13 14:06:33 +0800

    feat：默认参数

--- platform-jobs/platform-job-spark/src/main/java/com/wxf/App.java
+++ platform-jobs/platform-job-spark/src/main/java/com/wxf/App.java
@@ -1,13 +1,25 @@
 package com.wxf;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("App")
+                .master("local")
+                .getOrCreate();
+
+
+
+        spark.stop();
     }
 }
